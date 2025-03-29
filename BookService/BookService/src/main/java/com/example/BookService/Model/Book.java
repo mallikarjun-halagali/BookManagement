ca4ab1f4774d9dc8902808284986392925de9606
mallikarjun-halagali <88491807+mallikarjun-halagali@users.noreply.github.com>
2025-03-29 21:16:46 +0530

    Added the mysql dependencies and configaration file and book entity

--- BookService/BookService/src/main/java/com/example/BookService/Model/Book.java
+++ BookService/BookService/src/main/java/com/example/BookService/Model/Book.java
@@ -1,15 +1,43 @@
 package com.example.BookService.Model;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "books" , uniqueConstraints = @UniqueConstraint(columnNames = "isbn"))
+@Builder
 @Data
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank
+    @Size(min = 1, max = 255)
     private String title;
+
+
+    @NotBlank
+    @Size(min = 1, max = 255)
     private String author;
+
+
+    @NotBlank
+    @Pattern(regexp = "^(97(8|9))?\\d{9}(\\d|X)$", message = "Invalid ISBN format")
     private String isbn;
-    private String price;
+
+    private BigDecimal price;
+    @Pattern(regexp = "^(http|https)://.*$", message = "Invalid URL format")
     private String coverUrl;
-    private String publishedDate;
+
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false)
+    private LocalDate publishedDate;
+    @Size(max = 2000)
     private String description;
+
 }
